15a83a055bf09878191e06750ae7cb72fbf3c4e5
Tom Lemmers <geomazzix@gmail.com>
2020-02-01 21:24:26 +0100

    Added transport mechanisms.

---
+++ GGJ2020/Assets/Scripts/TransportBeltIn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportBeltIn : MonoBehaviour
+{
+    [SerializeField] private Transform m_Target;
+    [SerializeField] private Transform m_SpawnPos;
+    [SerializeField] private float m_BeltSpeed = 1;
+
+    public void SpawnNewInstance(GameObject gm)
+    {
+        Instantiate(gm, m_SpawnPos.position, Quaternion.identity);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, m_Target.position, m_BeltSpeed * Time.deltaTime);
+    }
+}
---
+++ GGJ2020/Assets/Scripts/TransportBeltOut.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TransportBeltOut : MonoBehaviour
+{
+    [SerializeField] private Transform m_Target;
+    [SerializeField] private float m_BeltSpeed = 1;
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, m_Target.position, m_BeltSpeed * Time.deltaTime);
+
+        if (Mathf.Abs((other.transform.position - m_Target.position).magnitude) <= 0.5f)
+            Destroy(other.gameObject);
+    }
+}
