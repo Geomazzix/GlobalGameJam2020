150a15693816fc3ee46be5660c995edc6c94acd0
Tom Lemmers <geomazzix@gmail.com>
2020-02-01 03:23:34 +0100

    Added the 4 basic items.

---
+++ GGJ2020/Assets/Scripts/Item.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Item types.
+/// </summary>
+public enum EItem
+{
+    METAL = 0,
+    WOOD,
+    CLOTH,
+    RUBBER
+}
+
+/// <summary>
+/// Represents an interactive item in the game for the player, used in order to create parts to repair toys.
+/// </summary>
+public class Item : MonoBehaviour, I_PickupItem
+{
+    private static int m_ItemCount = 0;
+    private int m_Id;
+
+    [SerializeField] private string m_Name;
+    [SerializeField] private EItem m_Type;
+
+    public int Id => m_Id;
+    public string Name => m_Name;
+    public EItem Type => m_Type;
+
+    private void Awake()
+    {
+        m_Id = m_ItemCount;
+        ++m_ItemCount;
+    }
+}
