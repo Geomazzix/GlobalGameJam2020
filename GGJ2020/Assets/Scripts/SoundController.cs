5f108c8321d549812178c6d67b0c51430e46757c
Olivier Candel <oliviercandel@hotmail.com>
2020-02-02 14:51:00 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- GGJ2020/Assets/Scripts/SoundController.cs
+++ GGJ2020/Assets/Scripts/SoundController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
+
+    public GameObject audioSource;
+
     private void Awake()
     {
         DontDestroyOnLoad(this);
@@ -11,12 +14,30 @@ public class SoundController : MonoBehaviour
 
     public void PlayAudioSource(AudioClip sfx, Vector3 position)
     {
-        GameObject gm = new GameObject();
-        gm.transform.position = position;
+        GameObject gm = Instantiate(new GameObject(), position, Quaternion.identity);
         AudioSource source = gm.AddComponent<AudioSource>();
         source.clip = sfx;
         source.minDistance = 0.5f;
         source.maxDistance = 4;
         source.Play();
+        playingEmitters.Add(gm);
+        Invoke("deleteSoundSource", sfx.length + 0.5f);
     }
+
+    public void deleteSoundSource()
+    {
+        foreach(GameObject obj in playingEmitters)
+        {
+            if (obj.GetComponent<AudioSource>().isPlaying == false)
+            {
+                for(int i = obj.transform.childCount; i > 0; i--)
+                {
+                    Destroy(obj.transform.GetChild(i - 1).gameObject);
+                }
+                Destroy(obj);
+            }
+        }
+    }
+
+    private List<GameObject> playingEmitters = new List<GameObject>();
 }
