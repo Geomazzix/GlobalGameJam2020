47ad44189e83095cbd20cb2d632f55e34d0c42de
Tom Lemmers <geomazzix@gmail.com>
2020-02-02 14:15:50 +0100

    SoundManager & main menu

---
+++ GGJ2020/Assets/Scripts/SoundController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void PlayAudioSource(AudioSource prefab, Vector3 position)
+    {
+        GameObject gm = Instantiate(prefab.gameObject, position, Quaternion.identity);
+        gm.GetComponent<AudioSource>().Play();
+    }
+}
