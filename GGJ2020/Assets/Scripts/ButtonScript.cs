6ccbcd78cdeecc342dc6e9c9cdec84ad83a31ed1
Olivier Candel <oliviercandel@hotmail.com>
2020-02-02 15:15:04 +0100

    latest main
    test and upload time

--- GGJ2020/Assets/Scripts/ButtonScript.cs
+++ GGJ2020/Assets/Scripts/ButtonScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class ButtonScript : MonoBehaviour, I_Interactable
 {
     public UnityEvent m_event;
- 
+    public AudioClip buttonSound;
+
     public void onInteract()
     {
+        FindObjectOfType<SoundController>().PlayAudioSource(buttonSound, transform.position);
         m_event.Invoke();
     }
 }
